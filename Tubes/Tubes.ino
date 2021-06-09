83d9dcf4071d940124a09f4d431be7bd27d95809
XwyzWorm <pratamaazmi1@gmail.com>
2021-06-09 11:55:19 +0700

    Finalisasi Kodingan

--- Tubes/Tubes.ino
+++ Tubes/Tubes.ino
@@ -1,35 +1,34 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+
+// Define the pin so its easy to understand
 #define pirPIN 16
 #define ledPin 5
 #define relayPin 4
 #define potentioPin 0
-#define photoResistorPin 12
+#define photoResistorPin 2
 
-int calibrationTime = 30;
-long unsigned int lowIn;
-long unsigned int pause = 500;
-boolean lockLow = true;
-boolean takeLowTime;
 
 int potentioValue; 
   
 int A0Pin = A0;
 
-
 int lightIntensityThres =70;
 int reading , bright ;
 
 // Update these with values suitable for your network.
+// Digunakan untuk inisiasi network
 const char* ssid =         "azmi";
 const char* password =    "passwordbaru000";
-const char* mqtt_server = "192.168.0.113";   /// example 192.168.0.19
+const char* mqtt_server = "192.168.0.113";   /// Menggunakan alamat IP Laptop ini , dikarenakan telah terinstall mosquito
 
+// Inisiasi Objects Wifi dan PubSub 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
+
+// Inisiasi Value dan msg untuk di pass pada method snprintf 
 char msg[50];
-int value = 0;
+int value = 1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
@@ -37,7 +36,6 @@ void setup() {
   pinMode(photoResistorPin, INPUT);
   pinMode(potentioPin,INPUT);
   pinMode(relayPin, OUTPUT);
-  
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -65,15 +63,14 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 void photodiode() {
-  reading=analogRead(A0);
+  reading=analogRead(potentioPin);
 
   if(reading > lightIntensityThres) {
       map(reading,0,1023,0,255);
       String readd= String(reading);
       const char* convertedRead = readd.c_str();
       snprintf (msg, 75, convertedRead,value);
-      Serial.print("Publish message for PhotoResistor: ");
-      Serial.println(msg);
+      //Serial.println("Publish message for PhotoResistor: ");
       client.publish("event/detectphotoresistor", msg);
 
   }
@@ -91,18 +88,19 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   // Switch on the LED if an 1 was received as first character
-  Serial.println("Sampis");
-  Serial.println((char)payload[0]);
   if ((char)payload[0] == '0') {
      Serial.println("LOW");
-    digitalWrite(ledPin, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
+     //digitalWrite(ledPin, LOW);  
+     Serial.println("Trash can currently Closed");
+     
   } 
 
  if ((char)payload[0] == '1') {
     Serial.println("HIGH");
-    digitalWrite(pirPIN, HIGH);  // Turn the LED off by making the voltage HIGH
+
+   // digitalWrite(ledPin,HIGH);
+    Serial.println("Trash can currently Open");
+    // Turn the LED off by making the voltage HIGH
   }
 
 }
@@ -118,9 +116,13 @@ void reconnect() {
     if (client.connect(clientIds.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("event/hello", "hello world,Connected");
+      client.publish("event/detectPIR", "hello world,Connected");
       // ... and resubscribe
-      client.subscribe("event/hello");
+      client.subscribe("event/detectPIR");
+            
+      
+      
+      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -138,25 +140,24 @@ void loop() {
   }
   client.loop();
 
-  long now = millis();
-  long lastMsg = 0;
-   
-    Serial.print("Pirpin: ");
-    Serial.println(digitalRead(pirPIN));
     if(digitalRead(pirPIN) == HIGH) {
-       photodiode();
-      Serial.println("Motion Deteced");
-      potentioValue = analogRead(potentioPin);
+      photodiode();
+      delay(100);
+      Serial.println("Motion Detected");
+      potentioValue = analogRead(potentioPin);    
+      digitalWrite(pirPIN, HIGH);
+      digitalWrite(relayPin,LOW);
       potentioValue = map(potentioValue, 0, 1023, 0, 255);
-      Serial.print("Potentio val :");
-      Serial.println(potentioValue);
+      
       if(potentioValue > 100) {
+        
         String readdPotentio= String(potentioValue);
         const char* convertedReadPotentio = readdPotentio.c_str();
         snprintf (msg, 75, convertedReadPotentio,value);
-        Serial.print("Publish message for Potentio: ");
-        Serial.println(msg);
+        //Serial.println("Publish message for Potentio: ");
         client.publish("event/potentioMeter", msg);
+        delay(100);
+        
         digitalWrite(ledPin,1);
         
         
@@ -165,17 +166,15 @@ void loop() {
         String readdPotentio= String(potentioValue);
         const char* convertedReadPotentio = readdPotentio.c_str();
         snprintf (msg, 75, convertedReadPotentio,value);
-        Serial.print("Publish message for Potentio: ");
-        Serial.println(msg);
+        //Serial.println("Publish message for Potentio: ");
         client.publish("event/potentioMeter", msg);
+        delay(100);
         analogWrite(ledPin,potentioValue);
+        
         }
       
-      digitalWrite(relayPin,0);
-      ++value;
       snprintf (msg, 75, "1", value);
-      Serial.print("Publish message: ");
-      Serial.println(msg);
+      //Serial.println("Publish message for PIR: ");
       client.publish("event/detectPIR", msg);
       delay(50);    
    
@@ -183,19 +182,15 @@ void loop() {
     
   if(digitalRead(pirPIN) == LOW) {
         
-        lockLow =true;
-        digitalWrite(ledPin,0);
-        digitalWrite(relayPin,1);
         Serial.println("Motion Ended");
         delay(50);
-     
-        ++value;
+        digitalWrite(pirPIN, LOW);
+        digitalWrite(ledPin, LOW);
+        digitalWrite(relayPin,HIGH);
         snprintf (msg, 75, "0", value);
-        Serial.print("Publish message: ");
-        Serial.println(msg);
+        //Serial.println("Publish message for PIR: ");
         client.publish("event/detectPIR", msg);
         }
     
-  
   delay(5000);
 }
